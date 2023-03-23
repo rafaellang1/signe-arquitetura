8e060b6fd9312716a760ab5fee33b687d83fdbc2
rafaellang1 <rafael.lang9@gmail.com>
2023-03-23 16:55:19 -0400

    responsiveness adjustments

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ function menuShow() {
     
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        document.querySelector('.icon').src="imagens/icons/menu-alt-04-svgrepo-com.svg";
+
+        document.querySelector('.header').style.background="transparent";
+
     }else {
         menuMobile.classList.add('open');
+        document.querySelector('.icon').src="imagens/icons/close-sm-svgrepo-com.svg";
+        
+        document.querySelector('.header').style.background="rgba(18, 41, 57, 0.900)"
     }
 }
